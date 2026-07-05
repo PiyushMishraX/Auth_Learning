0da48cc7782864ad49c5043c30919d669862a8d2
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-07-05 22:54:58 +0530

    Create authcontroller , explain difference in crypto vs bcrypt and explain sha256

--- 01_user_registration/src/controllers/auth.controller.js
+++ 01_user_registration/src/controllers/auth.controller.js
@@ -1,9 +1,26 @@
-
+import userModel from "../models/user.model"
+import crypto from "crypto" // inbuilt in the nodejs similar t bcrypt used for encryption etc
 
 async function register(req, res) {
 
     const { username, email, password} = req.body
 
+    const isAlreadtRegistered = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    if(isAlreadtRegistered){
+        res.status.json(409).json({
+            message: "Username or email already exists"
+        })
+    }
+
+    //  creating password hash // crypto
+    const hashedPassword = crypto.createHash("sha256").update(password).digest("hex") 
+    //  it performs a SHA-256 cryptographic hash on the provided password string and returns the result as a hexadecimal string. // sha-256 is algorithm creating hash of a fixed-length ( 256 bit is current standard )
 
     
 }
--- 01_user_registration/src/routes/auth.routes.js
+++ 01_user_registration/src/routes/auth.routes.js
@@ -5,6 +5,6 @@ const authRouter = Router(); // this file only for api declaration
 /**
  * POST /api/auth/register
  */
-authRouter.post("/register")
+authRouter.post("/register", )
 
 export default authRouter
