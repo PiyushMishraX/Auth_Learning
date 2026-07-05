459444dced9c7fe08c184a3e585283a81677c0b3
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-07-05 21:50:19 +0530

    FIx dns issue for type: module server to connect to mongo db

--- 01_user_registration/server.js
+++ 01_user_registration/server.js
@@ -2,6 +2,11 @@ import app from "./src/app.js"; // import needs the .js , require statement do n
 // import is newer , adding .js is better because it reduces confusion and conflicts
 import connectDB from "./src/config/database.js"
 
+
+// fix for "type":"module"
+import dns from "dns"
+dns.setServers(["1.1.1.1","8.8.8.8"])
+
 connectDB();
 
 app.listen(3000, ()=>{
