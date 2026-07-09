060eef19aeffa40597b3eac99c10c4c35115793d
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-07-09 23:02:55 +0530

    Create refresh and access token and create sotrage system for refresh token to be stored in cookies

--- 01_user_registration/src/app.js
+++ 01_user_registration/src/app.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import morgan from 'morgan'
 import authRouter from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev")) 
 // "dev" have limited feature that we need for now , different modes have different features
+app.use(cookieParser())
 
 app.use("/api/auth", authRouter);
+
 
 // app.use("/",(req,rest)=>{
 //     console.log("hello");
--- 01_user_registration/src/controllers/auth.controller.js
+++ 01_user_registration/src/controllers/auth.controller.js
@@ -3,6 +3,86 @@ import crypto from "crypto" // inbuilt in the nodejs similar t bcrypt used for e
 import jwt from "jsonwebtoken"  // need jwt secret to decrypt the token to verify if it is genrate by our server or not
 import config from "../config/config.js"
 
+// export async function register(req, res) {
+
+//     const { username, email, password} = req.body
+
+//     const isAlreadtRegistered = await userModel.findOne({
+//         $or: [
+//             { username },
+//             { email }
+//         ]
+//     })
+
+//     if(isAlreadtRegistered){
+//         res.status(409).json({
+//             message: "Username or email already exists"
+//         })
+//     }
+
+//     //  creating password hash // crypto
+//     const hashedPassword = crypto.createHash("sha256").update(password).digest("hex") 
+//     //  it performs a SHA-256 cryptographic hash on the provided password string and returns the result as a hexadecimal string. // sha-256 is algorithm creating hash of a fixed-length ( 256 bit is current standard )
+
+//     const user = await userModel.create({
+//         username,
+//         email,
+//         password: hashedPassword
+//     })
+//     // till this line the user data is saved in databse 
+//     // now server will send token to user // using ( jsonwebtoken )
+
+
+//     const token = jwt.sign({
+//         id: user._id,
+//     }, config.JWT_SECRET, 
+//         {
+//             // expiresIn: "1h"
+//             expiresIn: "1d" // even one day is considered too lnog for a token to expire
+//         }
+//     )
+//     // 201 when some resource is server side ( db value ) is created after user request
+//     res.status(201).json({
+//         message: "User registered successfully",
+//         user: {
+//             username: user.username,
+//             email: user.email,
+//         },
+//         token
+//     })
+    
+
+    
+// }
+
+// export async function getMe(req, res) {
+//     const token = req.headers.authorization.split(" ")[ 1 ]
+
+//     if(!token){
+//         return res.status(401).json({
+//             message: "Unauthorized"
+//         })
+//     }
+
+//     const decoded = jwt.verify(token, config.JWT_SECRET) // have the decoded data from the token
+//     // console.log(decoded);
+
+//     const user = await userModel.findById(decoded.id)
+//     // console.log(user);
+
+//     res.status(200).json({
+//         message: "User featched successfully",
+//         user:{
+//             username: user.username,
+//             email: user.email
+//         }
+//     })
+    
+    
+// }
+
+
+
 export async function register(req, res) {
 
     const { username, email, password} = req.body
@@ -20,35 +100,47 @@ export async function register(req, res) {
         })
     }
 
-    //  creating password hash // crypto
     const hashedPassword = crypto.createHash("sha256").update(password).digest("hex") 
-    //  it performs a SHA-256 cryptographic hash on the provided password string and returns the result as a hexadecimal string. // sha-256 is algorithm creating hash of a fixed-length ( 256 bit is current standard )
+
 
     const user = await userModel.create({
         username,
         email,
         password: hashedPassword
     })
-    // till this line the user data is saved in databse 
-    // now server will send token to user // using ( jsonwebtoken )
 
 
-    const token = jwt.sign({
+    const accessToken = jwt.sign({
         id: user._id,
     }, config.JWT_SECRET, 
         {
-            // expiresIn: "1h"
-            expiresIn: "1d" // even one day is considered too lnog for a token to expire
+            expiresIn: "15m" 
         }
     )
-    // 201 when some resource is server side ( db value ) is created after user request
+
+    const refreshToken = jwt.sign({
+        id: user._id,
+    }, config.JWT_SECRET, 
+        {
+            expiresIn: "7d" 
+        }
+    )
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,// client side js can not read the cookies data
+        secure:true,
+        sameSite: "Strict",
+        maxAge: 7 * 24 * 60 * 6 * 1000 // 7 days
+    })
+
     res.status(201).json({
         message: "User registered successfully",
         user: {
             username: user.username,
             email: user.email,
         },
-        token
+        // token: accessToken
+        accessToken
     })
     
 
@@ -64,11 +156,8 @@ export async function getMe(req, res) {
         })
     }
 
-    const decoded = jwt.verify(token, config.JWT_SECRET) // have the decoded data from the token
-    // console.log(decoded);
-
+    const decoded = jwt.verify(token, config.JWT_SECRET) 
     const user = await userModel.findById(decoded.id)
-    // console.log(user);
 
     res.status(200).json({
         message: "User featched successfully",
