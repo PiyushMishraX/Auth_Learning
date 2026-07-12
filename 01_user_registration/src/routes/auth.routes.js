c26f70439ee6dd59cca92de3dc5f4086b28d71c9
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-07-12 20:16:46 +0530

    Create login feature with refresh token,  accessToken and session generation

--- 01_user_registration/src/routes/auth.routes.js
+++ 01_user_registration/src/routes/auth.routes.js
@@ -9,6 +9,11 @@ const authRouter = Router(); // this file only for api declaration
 authRouter.post("/register", authController.register )
 
 /**
+ * POST /api/auth/login
+ */
+authRouter.post("/login", authController.login )
+
+/**
  * GET /api/auth/get-me
  */
 authRouter.get("/get-me", authController.getMe) 
